a1281580794dcf0d21067bc412c4bde1d88e9b37
Taisiya Gorodnicheva <taisiya-1992@nail.ru>
2025-10-08 22:18:44 +0300

    обавлены шаблоны и ссылки для просмотра и редактирования профиля пользователя

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
+    path('profile/', views.profile_view, name='profile_view'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
 ]
